1c9d5410a08c6aeea5077a3a2c9c1592aafdb22d
dksewell <dksewell@uiowa.edu>
2025-12-23 15:03:51 -0600

    Fixed bug in lm_b.  lm_b was erroneously sticking in a sd_y in the coefficients default prior precision matrix.

--- R/lm_b.R
+++ R/lm_b.R
@@ -66,7 +66,7 @@
 #'  to more than a 5 standard deviation in the mean of \eqn{y}."  If we then 
 #'  set the prior on the intercept to be flat, this leads to
 #'  \deqn{
-#'    V^{-1/2} = 2.5s_y\times diag(1,s_{x_1},\ldots,s_{x_p}),
+#'    V^{-1/2} = 2.5\times diag(1/s_y,s_{x_1},\ldots,s_{x_p}),
 #'  }
 #'  where \eqn{s_y} is the standard deviation of \eqn{y}, and \eqn{s_{x_j}} is 
 #'  the standard deviation of the \eqn{j^{th}} covariate.
@@ -256,6 +256,8 @@ lm_b = function(formula,
     if(missing(prior_beta_mean)){
       message("\nThe mu hyperparameter in the normal prior is not specified.  It will be set automatically to 0.\n")
       mu = rep(0,p)
+      if(all(dplyr::near(X[,1],1.0)))
+        mu[1] = mean(y)
     }else{
       mu = prior_beta_mean
     }
@@ -275,7 +277,7 @@ lm_b = function(formula,
       message("\nThe V hyperparameter in the normal prior is not specified.  It will be set automatically to 4/25Diag(s^2_{X_j})")
       if(ncol(X) > 1){
         V = 
-          diag(1.0 / c(2.5 * s_y,2.5 * s_y / s_j)^2)
+          diag(1.0 / c(2.5 * s_y,2.5 / s_j)^2)
       }else{
         V = 
           matrix(1.0 / (2.5 * s_y)^2,1,1)
